d121566ebff868a95016e7ccfc9d390c36845d36
Ram Kumar R <ramkumar.R@inatech.com>
2025-05-17 17:52:57 +0530

    create report with isdeleted

--- app/models/models.py
+++ app/models/models.py
@@ -99,6 +99,7 @@ class Report(Base):
     description = Column(String)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    is_deleted = Column(Boolean, default=False, nullable=False)
 
 class User(Base):
     __tablename__ = "users"
